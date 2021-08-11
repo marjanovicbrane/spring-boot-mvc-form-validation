20e681babf3204fc558b2a75f93de50b85623c86
Brane Marjanovic <marjanovicbrane93@gmail.com>
2021-08-11 14:10:08 +0200

    I created Spring Boot app MVC FORM VALIDATION, using Hibernate Validator API.In .pom file I added all dependencies for validator, MVC, dev tool, jasper and jstl.I also added in properties file application.properties configuration for JSP View Resolver.

---
+++ src/main/java/com/brane/form/SpringBootMvcFormValidationApplication.java
@@ -0,0 +1,13 @@
+package com.brane.form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcFormValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcFormValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brane/mvc/form/validation/SpringBootMvcFormValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brane.mvc.form.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcFormValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
