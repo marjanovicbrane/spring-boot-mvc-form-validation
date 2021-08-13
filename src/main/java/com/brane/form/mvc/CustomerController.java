bfd37b79e49516d7a47e66f063ab08d18d8b5108
Brane Marjanovic <marjanovicbrane93@gmail.com>
2021-08-13 16:09:16 +0200

    I created a new method in the classCustomer Controller.I also used annotation @InitBinder to trim all white spaces from the left and from the right side.If string only have white space, trim it to null.InitBinder pre-process all web requests coming into our Controller.

--- src/main/java/com/brane/form/mvc/CustomerController.java
+++ src/main/java/com/brane/form/mvc/CustomerController.java
@@ -17,6 +17,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CustomerController {
 
 	
+	//We have a problem here, for example, if we try to enter for the first name Brane and
+	//for the last name all white spaces, this will work and we will process the form, which is wrong.
+	//1.@InitBinder pre-process all web requests coming into our Controller.
+	//2.This method removes all whitespaces, from the left and from the right side.
+	//3.If string only have white space, trim it to null.
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		
+		//This object removes whitespace from the left and from the right side.
+		//true value means trim string to null if is all whitespace.
+		StringTrimmerEditor StringTrimmerEditor=new StringTrimmerEditor(true);
+		
+		//And we need to register this as a custom editor.
+		//For every string class, apply StringTrimmerEditor.
+		dataBinder.registerCustomEditor(String.class, StringTrimmerEditor);
+	}
+	
+	
+	
 	//method for showing the form, with parameter Model
 	@RequestMapping("/showForm")
 	public String showForm(Model theModel) {
@@ -36,6 +55,8 @@ public class CustomerController {
 		public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, 
 								  BindingResult theBindingResult) {
 			
+			//Just for debugging, we are going to print out this information.
+			System.out.println("Last name: |"+theCustomer.getLastName()+"|"+"First name: |"+theCustomer.getFirstName()+"|");
 			
 			//We are using now BindingResult object to see if we had errors.
 			//If we had errors, we want to return user back to form customer-form,
